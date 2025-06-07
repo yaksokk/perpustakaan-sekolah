72b5da73a804fc190f6b6b83e676ca16518660c0
ssamueluntu <yakusoku751@gmail.com>
2025-06-07 13:00:48 +0800

    membuat koleksi buku petugas dan pengguna

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,16 @@
 import Dashboard from './dashboard/dashboard'
-import KoleksiBuku from './Operator/koleksi-buku/koleksiBuku'
+import Users from './Admin/users/users'
 import BorrowList from './Operator/borrow-list/borrowList'
 import ReturnedList from './Operator/returned-list/ReturnedList'
 import SelectedBooks from './Operator/selected-books/selectedBooks'
-import Pengembalian from './User/pengembalian/pengembalian'
+import { default as KoleksiBukuOperator } from './Operator/koleksi-buku/koleksiBuku';
 import Peminjaman from './User/peminjaman/peminjaman'
-import Users from './Admin/users/users'
+import Pengembalian from './User/pengembalian/pengembalian'
+import { default as KoleksiBukuUser } from './User/koleksi-buku/koleksiBuku';
 export {
     Dashboard,
-    KoleksiBuku,
+    KoleksiBukuOperator,
+    KoleksiBukuUser,
     Peminjaman,
     Pengembalian,
     SelectedBooks,
