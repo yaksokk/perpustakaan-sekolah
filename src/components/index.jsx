d67e2466c4fbc54102aa2f21ee47e960db25101f
ssamueluntu <yakusoku751@gmail.com>
2025-06-07 10:26:07 +0800

    menambahkan Table

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,5 @@
 import Navbar from './layout/navbar/navbar'
 import Sidebar from './layout/sidebar/sidebar'
 import PageRouter from './layout/pageRouter';
-export { Navbar, Sidebar, PageRouter };
+import Table from './shared/table/table';
+export { Navbar, Sidebar, PageRouter, Table };
